69a1c952f7254b14007bf7d6ee2841ae942d6a78
odradek <odradek@rlab.be>
2023-04-14 10:04:13 -0300

    add account

--- send_slack_lambda_test.py
+++ send_slack_lambda_test.py
@@ -10,22 +10,24 @@ from urllib.error import URLError, HTTPError
 
 
 # The base-64 encoded, encrypted key (CiphertextBlob) stored in the kmsEncryptedHookUrl environment variable
-ENCRYPTED_HOOK_URL = os.environ['kmsEncryptedHookUrl']
+# ENCRYPTED_HOOK_URL = os.environ['kmsEncryptedHookUrl']
 # The Slack channel to send a message to stored in the slackChannel environment variable
-SLACK_CHANNEL = os.environ['slackChannel']
+# SLACK_CHANNEL = os.environ['slackChannel']
 
-HOOK_URL = "https://" + boto3.client('kms').decrypt(
-    CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL)
-    )['Plaintext'].decode('utf-8')
+# HOOK_URL = "https://" + boto3.client('kms').decrypt(
+    # CiphertextBlob=b64decode(ENCRYPTED_HOOK_URL)
+    # )['Plaintext'].decode('utf-8')
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# logger.setLevel(logging.INFO)
 
 
-def lambda_handler(event, context):
-    logger.info("Event: " + str(event))
-    message = json.dumps(event['Records'][0]['Sns']['Message'])
-    logger.info("Message: " + str(message))
+def lambda_handler():
+    # logger.info("Event: " + str(event))
+    # message = json.dumps(event['Records'][0]['Sns']['Message'])
+    # logger.info("Message: " + str(message))
+    f = open('sns.json')
+    data = json.load(f)
 
     alarm_name = json.dumps(data['Records'][0]['Sns']['Message']['AlarmName'])
     old_state = json.dumps(data['Records'][0]['Sns']['Message']['OldStateValue'])
@@ -36,15 +38,17 @@ def lambda_handler(event, context):
     payload = {"text": " %s state is now %s: %s in Account %s" % (alarm_name, new_state, reason, account)}
     headers = {'Content-Type': "application/json"}
 #    print(HOOK_URL)
-
-    try:
-        req = requests.post(HOOK_URL,
-            json.dumps(payload), 
-            headers=headers)
-    except ValueError:
-        print("Couldn't send the message")
-    finally:
-        return {
-            'statusCode': 200,
-            'body': json.dumps('Code Run Succesfully')
-        }
+    print(payload)
+    # try:
+        # req = requests.post(HOOK_URL,
+            # json.dumps(payload), 
+            # headers=headers)
+    # except ValueError:
+        # print("Couldn't send the message")
+    # finally:
+        # return {
+            # 'statusCode': 200,
+            # 'body': json.dumps('Code Run Succesfully')
+        # }
+if __name__ == '__main__':
+    lambda_handler()
